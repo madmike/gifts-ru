b5ec32815cbe5f18f0ec19241864e79a8e5b4f99
Mikhail Volkov <mvolkov@openweather.co.uk>
2025-02-04 13:14:13 -0300

    update mongoose connection

--- src/services/database.module.ts
+++ src/services/database.module.ts
@@ -1,18 +1,34 @@
 import mongoose from 'mongoose';
 import { Module } from '@nestjs/common';
 
+const constructMongoURI = (envVars: any) => {
+  let uri = 'mongodb://';
+
+  if (envVars.DB_USER && envVars.DB_PASSWORD) {
+    uri += `${encodeURIComponent(envVars.DB_USER)}:${encodeURIComponent(envVars.DB_PASSWORD)}@`;
+  }
+
+  uri += `${envVars.DB_HOST || 'localhost'}:${envVars.DB_PORT || 27017}/${envVars.DB_NAME || 'merch'}`;
+
+  const queryParams: string[] = [
+    `authSource=${encodeURIComponent(envVars.MONGO_AUTH_SOURCE || 'admin')}`,
+  ];
+  if (envVars.MONGO_TLS === 'true') {
+    queryParams.push('ssl=true');
+  }
+
+  if (queryParams.length > 0) {
+    uri += `?${queryParams.join('&')}`;
+  }
+
+  return uri;
+};
+
 const databaseProviders = [
   {
     provide: 'DB',
     useFactory: (): Promise<typeof mongoose> => {
-      const creds =
-        process.env.DB_USER && process.env.DB_PASSWORD
-          ? `${process.env.DB_USER}:${process.env.DB_PASSWORD}@`
-          : '';
-      const host =
-        (process.env.DB_HOST || 'localhost') +
-        (process.env.DB_PORT ? `:${process.env.DB_PORT}` : '');
-      const url = `mongodb://${creds}${host}/${process.env.DB_NAME}?authSource=admin`;
+      const url = constructMongoURI(process.env);
       return mongoose.connect(url, { maxPoolSize: 5 });
     },
   },
